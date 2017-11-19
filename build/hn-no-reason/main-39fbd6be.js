4621b65508644312e0e54e4c74fc3863278248ca
Mike Elliot <elliotecweb@gmail.com>
2017-11-18 17:49:00 -0700

    finish hn post and build

--- build/hn-no-reason/main-39fbd6be.js
+++ build/hn-no-reason/main-39fbd6be.js
@@ -1,45 +1,3 @@
----
-title: A Hacker News Reader For No Reason
-date: 2017-11-18 13:23 MST
-tags: javascript, hacker news
-published: true
----
-
-The title is only half true.
-
-The other night I was thinking about how my experience being fully entrenched in React and the modern JavaScript ecosystem lately might have influenced how I'd write an app by hand in vanilla HTML, CSS, and JS with no libraries or build tools.
-
-Within the same hour I was looking at [Hacker News](https://news.ycombinator.com/) and some of it's readers, noting how they all kind of sucked as far as UX (including HN itself - still without mobile support and only recently adding comment toggling), and looked up [their API](https://hacker-news.firebaseio.com/v0/).
-
-Clearly this presented an opportunity to build my own HN reader in the lightest way I could - fully client side, no libraries, totally vanilla.
-
-So I did it in 2 files, 99 lines of JavaScript at 4.4 KB, and 99 lines of HTML/CSS at 3.1 KB. To save you the arithmetic, that's 198 total lines of code at 7.5 KB.
-
-<a href="/hn-no-reason" target='_blank'>
-    <img src="../images/hn-no-reason.png" alt='HN App Image' />
-    <h2>Go to the app!</h2>
-</a>
-
-### Features
-
-- Gets the top 30 stories in order with their points, just like the front page of HN.
-- If the story has a URL, the title will take you to a new tab with the URL, otherwise it will toggle the text content of the story.
-- If the story has comments, there's a link to toggle the comments.
-- Each comment and their children are collapsible.
-- There's a link to the original story on HN, and to the user that submitted the story or comments.
-- Looks decent on all devices.
-- Super tiny and pretty fast.
-
-
-### The code
-
-I inlined the styles in `index.html`, but didn't minify anything. Since one of the goals I had with this was to keep it super small and light, I characteristically set an arbitrary limit of lines for myself at a number I figured seemed reasonable to get it within.
-
-I fudged some of my usual code style guidelines and disrespected the 80 char line rule I prefer to abide by a few times in order to hit the 99 line mark in both files, but nothing too crazy and I'm not crossing any real hard bad-practice lines.
-
-You can check out [the repo here](https://github.com/elliotec/hn-no-reason), but since it's only 99 lines, I've posted the contents of JS file below. Note the use of several ES6+ features and the resemblance to React and JSX with the templating.
-
-```javascript
 const state = {}
 const hnBaseUrl = 'https://hacker-news.firebaseio.com/v0'
 function fetchTopStories() {
@@ -139,14 +97,4 @@ function renderComments(comments, storyId) {
   })
 }
 fetchTopStories()
-```
-I played with minifying and inlining the JS, but it's only 4.4 KB in the first place and minifying it only brought it down to 3.9 which is pretty negligible. Having the script on the HTML page instead of in a separate file seemed to have no performance hit at all. I did the CSS inline because that's just how I started it, but I didn't measure the performance gain by splitting it out.
-
-### Limitations
-
-- Since I'm using modern ECMAScript features, only more modern browsers will support it, but my audience should be fine with that.
-- Only shows the top stories - no Ask HN, new, jobs, etc. unless they're on the front page. This is not a limitation to me personally since I spend 99% of my HN time just on the front page.
-- No login, upvote, or posting functionality. Unfortunately the HN API doesn't support this yet, but if/when it does, maybe I'll try to get the all that in another 99 lines.
-- I'm not a designer. This was styled with my typical aesthetic of a minimal dark theme with green all over. Probably not everyone's favorite but certainly mine, and it's mine, so whatever.
-
-There you have it. Maybe someone else will also find a reason to use A Hacker News Reader For No Reason.
+;
