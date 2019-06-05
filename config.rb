502b8d146695eaae673d27e4cd1c7bf1470eed33
Mike Elliott <melliott@nav.com>
2019-06-04 21:40:02 -0600

    fix svgout

--- config.rb
+++ config.rb
@@ -18,8 +18,18 @@ configure :build do
   # activate :minify_javascript, inline: true
   activate :minify_html
   activate :gzip
-  activate :imageoptim
   activate :asset_hash
+  activate :imageoptim do |options|
+    # Use a build manifest to prevent re-compressing images between builds
+    options.manifest = true
+    # Silence problematic image_optim workers
+    options.skip_missing_workers = true
+    # Cause image_optim to be in shouty-mode
+    options.verbose = false
+    # Setting these to true or nil will let options determine them (recommended)
+    options.nice = true
+    options.threads = true
+  end
 end
 
 Time.zone = 'America/Denver'
@@ -37,7 +47,7 @@ activate :blog do |blog|
 end
 activate :livereload
 activate :directory_indexes
-activate :syntax, line_numbers:false
+activate :syntax, line_numbers: false
 
 activate :deploy do |deploy|
   deploy.deploy_method = :git
